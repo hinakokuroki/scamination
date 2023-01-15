b525c2f9d0f94c1c34b5125a37d645beb75af202
Hinako Kuroki <pi.pi.hinako.29@gmail.com>
2023-01-15 16:58:58 +0900

    ドラッグテスト

--- sketch.js
+++ sketch.js
@@ -1,3 +1,5 @@
+let slitY;
+
 function setup() {
   window.addEventListener(
     "touchstart",
@@ -16,15 +18,28 @@ function setup() {
     }
   );
   createCanvas(windowWidth, windowHeight);
+  slitY = windowHeight - 40;
 }
 
 function draw() {
-  background(220);
-  stroke(255, 0, 0);
+  background(250);
+  stroke(100);
   text(windowWidth + "," + windowHeight, 50, 50);
+  rect(20, slitY, windowWidth - 40, windowHeight);
   if (mouseIsPressed) {
+    slitY = slitY + calcSlitY();
     fill(255);
     ellipse(mouseX, mouseY, 50, 50);
     text(windowWidth + "," + windowHeight, mouseX, mouseY - 50);
   }
+}
+
+function calcSlitY() {
+  let startY, moveY;
+  if (mouseIsPressed && count == 0) {
+    startY = mouseY;
+  } else if (mouseIsPressed) {
+    moveY = mouseY - startY;
+  }
+  return moveY;
 }
